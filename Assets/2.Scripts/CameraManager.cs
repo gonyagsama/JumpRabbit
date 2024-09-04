eaadb36ebd2c14565bbaa448221082a4166ab6f1
Leejaerin <aaa1967200521@gmail.com>
2024-09-04 15:50:50 +0900

    배경 흘러가기

--- Assets/2.Scripts/CameraManager.cs
+++ Assets/2.Scripts/CameraManager.cs
@@ -5,10 +5,16 @@ using UnityEngine;
 public class CameraManager : MonoBehaviour
 {
     public static CameraManager Instance;
+    [SerializeField] private SpriteRenderer bgSrdr;
+
+    float cameraWidth;
 
     public void Init()
     {
         Instance = this;
+        Camera camera = Camera.main;
+        float cameraHeight = camera.orthographicSize * 2f;
+        cameraWidth = cameraHeight * camera.aspect;
     }
     public void OnFollow(Vector2 targetPos)
     {
@@ -21,6 +27,13 @@ public class CameraManager : MonoBehaviour
         while (0.1f<Vector3.Distance(transform.position, targetPos))
         {
             transform.position = Vector3.Lerp(transform.position, targetPos, Time.deltaTime * DataBaseManager.Instance.followSpeed);
+            float bgRightX = bgSrdr.transform.position.x + bgSrdr.size.x;
+            float cameraRightX = Camera.main.transform.position.x + cameraWidth / 2;
+
+            if(bgRightX <= cameraRightX)
+            {
+                bgSrdr.size = new Vector2(bgSrdr.size.x + cameraWidth , bgSrdr.size.y);
+            }
             yield return null;
         }
     }
